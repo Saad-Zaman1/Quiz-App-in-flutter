a5af4ca61404dc8322bcf5f3b44cd88e60bf4c67
Saad-Zaman1 <saad.zaman5439@gmail.com>
2023-08-10 09:22:57 +0500

    creating quiz and question screen

---
+++ lib/questions.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatefulWidget {
+  const Questions({super.key});
+  @override
+  State<Questions> createState() {
+    return _QuestionsState();
+  }
+}
+
+class _QuestionsState extends State<Questions> {
+  @override
+  Widget build(context) {
+    return Text("data");
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return const Text("data");
+  }
+}
