4d30019dadb4407f5deec4817d0b705d9f804a4c
Saad-Zaman1 <saad.zaman5439@gmail.com>
2023-08-15 10:32:04 +0500

    done with switching screen using initState method

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:second_project/start_screen.dart';
+
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,6 +13,20 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   @override
   Widget build(context) {
-    return const Text("data");
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                Color.fromARGB(255, 5, 2, 204),
+                Color.fromARGB(146, 15, 2, 192),
+              ])),
+          child: const StartScreen(),
+        ),
+      ),
+    );
   }
 }
